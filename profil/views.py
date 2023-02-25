4e2369d3272cafabb094a5abcb7458977cf03a39
Clément <contact@cburdeyron.com>
2023-02-25 12:59:41 +0000

    create view

--- profil/views.py
+++ profil/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+'''
+Views for profil app
+'''
+from django.views.generic import TemplateView
 
-# Create your views here.
+from profil.models import Work, Education, Project, CategorieUse, Use, About
+
+class ProfilView(TemplateView):
+    '''
+    Class of profil view
+    '''
+    template_name = 'pages/page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfilView, self).get_context_data(**kwargs)
+        context['works'] = Work.objects.all().order_by('-order')
+        context['educations'] = Education.objects.all().order_by('-order')
+        context['projects'] = Project.objects.all()
+        context['categories'] = CategorieUse.objects.all()
+        context['uses'] = Use.objects.all()
+        context['abouts'] = About.objects.all()
+        return context
