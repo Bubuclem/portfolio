1c323acba70b06dc6044e3996da44a3475a3ded8
Clément <contact@cburdeyron.com>
2023-02-25 09:53:15 +0000

    Comment apps.py

--- back/apps.py
+++ back/apps.py
@@ -1,6 +1,11 @@
+'''
+Back app config
+'''
 from django.apps import AppConfig
 
-
 class BackConfig(AppConfig):
+    '''
+    Back app config
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'back'
