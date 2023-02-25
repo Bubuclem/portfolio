f35046e5b58ff22b3f6223d40a4479ee5e764489
Clément <contact@cburdeyron.com>
2023-02-25 10:14:33 +0000

    apps.py comment

--- front/apps.py
+++ front/apps.py
@@ -1,6 +1,11 @@
+'''
+Config for front app
+'''
 from django.apps import AppConfig
 
-
 class FrontConfig(AppConfig):
+    '''
+    Config for front app
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'front'
