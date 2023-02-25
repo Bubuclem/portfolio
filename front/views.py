786ae45d45e19ae8a368372d5a4afb3758a7c984
Clément <contact@cburdeyron.com>
2023-02-25 09:55:14 +0000

    comment views.py

--- front/views.py
+++ front/views.py
@@ -1,22 +1,28 @@
+'''
+Views for front app
+'''
 from django.shortcuts import redirect, render
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
 
 from front.models import SocialMedia, Tool, Address, ContactMe, AboutMe, Newsletter, Message
 from front.forms import contactForm, newsletterForm
 
+
 class BaseView(TemplateView):
     ''' 
-    Generic TemplateView 
+    Generic TemplateView
     Return context with social
     '''
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['social_media'] = SocialMedia.objects.all()
         return context
 
+
 class IndexPageView(BaseView):
     ''' 
-    Index Page 
+    Index Page
     Get foor first tools from database filtred by 'order'
     '''
     template_name = 'pages/front/index.html'
@@ -29,9 +35,10 @@ class IndexPageView(BaseView):
         return context
 
     ''' 
-    Post the newsletter 
+    Post the newsletter
     Check if the email exist in database
     '''
+
     def post(self, request, *args, **kwargs):
         form = newsletterForm(request.POST)
         if form.is_valid():
@@ -40,9 +47,10 @@ class IndexPageView(BaseView):
                 form.save()
         return redirect('/')
 
+
 class ToolsPageView(BaseView):
     ''' 
-    Tools Page 
+    Tools Page
     Get all tools from database
     '''
     template_name = 'pages/front/tools.html'
@@ -52,8 +60,11 @@ class ToolsPageView(BaseView):
         context['tools'] = Tool.objects.all()
         return context
 
+
 class AboutPageView(BaseView):
-    ''' About Page '''
+    ''' 
+    About Page
+    '''
     template_name = 'pages/front/about.html'
 
     def get_context_data(self, **kwargs):
@@ -61,9 +72,10 @@ class AboutPageView(BaseView):
         context['newsletter'] = newsletterForm()
         return context
 
+
 class ContactPageView(BaseView):
     ''' 
-    Contact Page 
+    Contact Page
     Get all addresses from database
     '''
     template_name = 'pages/front/contact.html'
@@ -75,31 +87,32 @@ class ContactPageView(BaseView):
         context['contacts'] = ContactMe.objects.all()
         return context
 
-    '''
-    Post the contact form
-    Check how many messsages about this email have been sent
-    if he has sent 5 messages, return error message
-    '''
     def post(self, request, *args, **kwargs):
+        '''
+        Post the contact form
+        Check how many messsages about this email have been sent
+        if he has sent 5 messages, return error message
+        '''
         form = contactForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             if Message.objects.filter(email=email).count() >= 5:
-                form.add_error('email', 'You have sent 5 messages about this email. Please try again later.')
+                form.add_error(
+                    'email', 'You have sent 5 messages about this email. Please try again later.')
             else:
                 form.save()
         return redirect('/contact/')
 
-''' 
-View 404 
-Return 404 page if page not found
-'''
 def page_not_found(request, exception):
+    ''' 
+    View 404 
+    Return 404 page if page not found
+    '''
     return render(request, 'pages/front/404.html', status=404)
 
-''' 
-View 500 
-Return 500 page if server error
-'''
 def server_error(request):
+    ''' 
+    View 500 
+    Return 500 page if server error
+    '''
     return render(request, 'errors/500.html', status=500)
