da0565a18c2daead9cd37473040eb7515d8e2e0a
Clément <contact@cburdeyron.com>
2023-02-25 09:52:21 +0000

    comment admin.py

--- back/admin.py
+++ back/admin.py
@@ -1,9 +1,15 @@
+'''
+Admin site for the back app
+'''
 from django.contrib import admin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import User
+from back.models import User
 from django.contrib.auth.admin import UserAdmin
 
 class UserTool(UserAdmin):
+    '''
+    User admin tool
+    '''
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
     search_fields = ('username', 'email', 'first_name', 'last_name')
@@ -12,7 +18,8 @@ class UserTool(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'email','picture_profile')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+        'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
