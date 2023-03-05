c2931a8c2e484b079e48a4f45af0127b19438e9e
Clément <contact@cburdeyron.com>
2023-03-05 11:56:22 +0000

    add honeypot

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('',include('profil.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('next/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
