1f5856f8eaec3fa67cbae3eff754f1d3a6836624
Clément <contact@cburdeyron.com>
2023-03-10 12:56:08 +0000

    404 & 505 pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -10,3 +10,6 @@ urlpatterns = [
     path('next/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+handler404 = 'portfolio.views.handler404'
+handler500 = 'portfolio.views.handler500'
