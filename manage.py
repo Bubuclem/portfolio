08fac1ae5147c21c98d7c347839e2fc3b9ba2336
Clément <25383654+Bubuclem@users.noreply.github.com>
2023-11-04 11:21:00 +0000

    Remove unnecessary code in manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.configurations.github')
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'Github')
     try:
         from configurations.management import execute_from_command_line
     except ImportError as exc:
